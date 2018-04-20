a05ab94110468998ba8e0ed94e43af00f0152b83
Christoph Iserlohn <christoph.iserlohn@innoq.com>
2018-04-20 12:34:25 +0200

    increase timeout

--- src/genserver/mining.erl
+++ src/genserver/mining.erl
@@ -40,8 +40,8 @@ proof(JsonStart, JsonEnd) ->
 		Any ->
 			io:format("Unknown message: ~p", [Any]),
 			{error, "Unknown message!"} %TODO: insert Any into error message!
-	after 15000 ->
-		io:format("No proof found in 15s. Timeout", []),
+	after 60000 ->
+		io:format("No proof found in 60s. Timeout", []),
 		{error, "Timeout!"}
 	end.
 
